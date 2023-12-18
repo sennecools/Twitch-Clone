216d92ce83fdab7551b5f0af67212bd220eb7f42
Senne Cools <senne.cools@student.odisee.be>
2023-12-18 04:00:25 +0100

    Add responsive behavior to Container component

--- app/(browse)/_components/container.tsx
+++ app/(browse)/_components/container.tsx
@@ -1,7 +1,31 @@
+'use client';
+
+import { cn } from '@/lib/utils';
+import { useSidebar } from '@/store/use-sidebar';
+import { useEffect } from 'react';
+import { useMediaQuery } from 'usehooks-ts';
+
 interface ContainerProps {
 	children: React.ReactNode;
 }
 
 export const Container = ({ children }: ContainerProps) => {
-	return <div>{children}</div>;
+	const matches = useMediaQuery('(max-width: 1024px)');
+	const { collapsed, onExpand, onCollapse } = useSidebar((state) => state);
+
+	useEffect(() => {
+		if (matches) {
+			onCollapse();
+		} else {
+			onExpand();
+		}
+	}, [matches, onCollapse, onExpand]);
+
+	return (
+		<div
+			className={cn('flex-1', collapsed ? 'ml-[70px]' : 'ml-[70px] lg:ml-60')}
+		>
+			{children}
+		</div>
+	);
 };
