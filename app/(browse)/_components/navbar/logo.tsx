9ec075e408a4620f1ab880d677f2a3dc3db99cdf
Senne Cools <senne.cools@student.odisee.be>
2023-12-18 03:36:56 +0100

    navbar and start sidebar

---
+++ app/(browse)/_components/navbar/logo.tsx
@@ -0,0 +1,25 @@
+import { cn } from '@/lib/utils';
+import { Poppins } from 'next/font/google';
+import Image from 'next/image';
+import Link from 'next/link';
+
+const font = Poppins({
+	subsets: ['latin'],
+	weight: ['200', '300', '400', '500', '600', '700', '800'],
+});
+
+export const Logo = () => {
+	return (
+		<Link href="/">
+			<div className="flex items-center gap-x-4 hover:opacity-75 transition">
+				<div className="bg-white rounded-full p-1 mr-12 shrink-0 lg:mr-0 lg:shrink">
+					<Image src="/spooky.svg" alt="StreamScape" height="32" width="32" />
+				</div>
+				<div className={cn('hidden lg:block', font.className)}>
+					<p className="text-lg font-semibold">StreamScape</p>
+					<p className="text-xs text-muted-foreground">let&apos;s play</p>
+				</div>
+			</div>
+		</Link>
+	);
+};
