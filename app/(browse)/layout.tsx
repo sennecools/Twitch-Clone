7e0e7231514b4ec29d705c6de67a6cd9ec15fc94
Senne Cools <senne.cools@student.odisee.be>
2023-12-20 12:58:38 +0100

    Fix typo in import statement

--- app/(browse)/layout.tsx
+++ app/(browse)/layout.tsx
@@ -1,14 +1,14 @@
 import { Suspense } from 'react';
 import { Container } from './_components/container';
 import { Navbar } from './_components/navbar';
-import { Sidebar, SiderbarSkeleton } from './_components/sidebar';
+import { Sidebar, SidebarSkeleton } from './_components/sidebar';
 
 const constBrowseLayout = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<>
 			<Navbar />
 			<div className="flex h-full pt-20">
-				<Suspense fallback={<SiderbarSkeleton />}>
+				<Suspense fallback={<SidebarSkeleton />}>
 					<Sidebar />
 				</Suspense>
 				<Container>{children}</Container>
