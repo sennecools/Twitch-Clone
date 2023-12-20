96193adec364bb381cf988b784f8e4bf715702f4
Senne Cools <senne.cools@student.odisee.be>
2023-12-20 11:35:14 +0100

    Add follow and unfollow functionality

--- actions/follow.ts
+++ actions/follow.ts
@@ -1,11 +1,35 @@
 'use server';
 
-import { followUser } from '@/lib/follow-service';
+import { followUser, unfollowUser } from '@/lib/follow-service';
+import { revalidatePath } from 'next/cache';
 
 export const onFollow = async (id: string) => {
 	try {
 		const followedUser = await followUser(id);
+		revalidatePath('/');
+
+		if (followedUser) {
+			revalidatePath(`/${followedUser.following.username}`);
+		}
+
+		return followedUser;
 	} catch (error) {
 		throw new Error('Internal error');
+	}
+};
+
+export const onUnfollow = async (id: string) => {
+	try {
+		const unfollowedUser = await unfollowUser(id);
+
+		revalidatePath('/');
+
+		if (unfollowedUser) {
+			revalidatePath(`/${unfollowedUser.following.username}`);
+		}
+
+		return unfollowedUser;
+	} catch (error) {
+		throw new Error('Internal Error');
 	}
 };
--- app/(browse)/[username]/_components/actions.tsx
+++ app/(browse)/[username]/_components/actions.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { onFollow } from '@/actions/follow';
+import { onFollow, onUnfollow } from '@/actions/follow';
 import { Button } from '@/components/ui/button';
 import { useTransition } from 'react';
 import { toast } from 'sonner';
@@ -12,20 +12,37 @@ interface ActionsProps {
 export const Actions = ({ isFollowing, userId }: ActionsProps) => {
 	const [isPending, startTransition] = useTransition();
 
-	const onClick = () => {
+	const handleFollow = () => {
 		startTransition(() => {
-			onFollow('123')
-				.then(() => toast.success('Followed the user'))
+			onFollow(userId)
+				.then((data) =>
+					toast.success(`You are now following ${data.following.username}`)
+				)
 				.catch(() => toast.error('Something went wrong'));
 		});
 	};
+
+	const handleUnfollow = () => {
+		startTransition(() => {
+			onUnfollow(userId)
+				.then((data) =>
+					toast.success(`You have unfollowed ${data.following.username}`)
+				)
+				.catch(() => toast.error('Something went wrong'));
+		});
+	};
+
+	const onClick = () => {
+		if (isFollowing) {
+			handleUnfollow();
+		} else {
+			handleFollow();
+		}
+	};
+
 	return (
-		<Button
-			disabled={isFollowing || isPending}
-			onClick={onClick}
-			variant="primary"
-		>
-			Follow
+		<Button disabled={isPending} onClick={onClick} variant="primary">
+			{isFollowing ? 'Unfollow' : 'follow'}
 		</Button>
 	);
 };
